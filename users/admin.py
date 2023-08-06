c6b0ab73786a63c8d0ad40fd958ffe706beaf798
ksun <ksusha1354@gmail.com>
2023-08-06 13:01:04 +0300

    Added fixtures and permission in admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+from mail_service.models import Mailing
 from users.models import User
 
 
@@ -9,3 +10,19 @@ from users.models import User
 @admin.register(User)
 class ClientAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'phone', 'avatar')
+
+
+class ABCAdmin(admin.ModelAdmin):
+    fields = ['status', 'user', ]
+
+    def get_fields(self, request, obj=None):
+        if request.user.groups.filter(name="Мененджер"):
+            return ['status', 'user', ]
+        elif request.user.is_superuser:
+            return [i.name for i in Mailing._meta.fields if i.name != "id"]
+        else:
+            return []
+
+
+admin.site.unregister(Mailing)
+admin.site.register(Mailing, ABCAdmin)
