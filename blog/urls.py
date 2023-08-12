0b0b3637b6d7c863f02a6c7a82013f11de5768a9
ksun <ksusha1354@gmail.com>
2023-08-13 02:14:29 +0300

    Redesigned cron

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from django.views.decorators.cache import cache_page
 
 from blog.apps import BlogConfig
-from blog.views import ArticleListView, ArticleCreateView, ArticleUpdateView, ArticleDeleteView, toggle_activity
+from blog.views import ArticleListView, ArticleCreateView, ArticleUpdateView, ArticleDeleteView, toggle_activity, \
+    ArticleDetailView
 
 app_name = BlogConfig.name
 
@@ -10,6 +11,7 @@ urlpatterns = [
     path('', cache_page(60)(ArticleListView.as_view()), name='list'),
     path('create/', ArticleCreateView.as_view(), name='create'),
     path('edit/<int:pk>', ArticleUpdateView.as_view(), name='edit'),
+    path('view/<int:pk>', ArticleDetailView.as_view(), name='view'),
     path('delete/<int:pk>', ArticleDeleteView.as_view(), name='delete'),
     path('published/<int:pk>', toggle_activity, name='toggle_activity'),
 ]
--- mail_service/cron.py
+++ mail_service/cron.py
@@ -15,16 +15,19 @@ def my_scheduled_job():
     tz = pytz.timezone('Europe/Moscow')
 
     for new_mailing in mailings:
+        print(datetime.now(tz).timestamp(), new_mailing.mailing_time.timestamp())
         clients = [client.email for client in Client.objects.filter(user=new_mailing.user)]
-        if new_mailing.mailing_time >= datetime.now(tz):
+        if new_mailing.mailing_time.timestamp() <= datetime.now(tz).timestamp():
             mail_subject = new_mailing.massage.email_body if new_mailing.massage is not None else 'Рассылка'
             message = new_mailing.massage.email_theme if new_mailing.massage is not None else 'Вам назначена рассылка'
             try:
                 send_mail(mail_subject, message, settings.EMAIL_HOST_USER, clients)
-                log = Log.objects.create(attempt_date=datetime.now(tz), status='Успешно', server_response='200')
+                log = Log.objects.create(attempt_date=datetime.now(tz), status='Успешно', server_response='200',
+                                         mailing=new_mailing)
                 log.save()
             except smtplib.SMTPException as err:
-                log = Log.objects.create(attempt_date=datetime.now(tz), status='Ошибка', server_response=err)
+                log = Log.objects.create(attempt_date=datetime.now(tz), status='Ошибка', server_response=err,
+                                         mailing=new_mailing)
                 log.save()
                 # raise err
             new_mailing.status = 3
